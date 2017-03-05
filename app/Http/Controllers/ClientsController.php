617a4db962db348777119bdea70581a589106a09
Marcos Barbosa <barbosa.mg@gmail.com>
2017-03-05 13:38:43 -0300

    Criando listagem de clientes

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -17,6 +17,10 @@ class ClientsController extends Controller
 	 */
 	public function index()
 	{
+		$clients = Client::all();
+		
+		return view('clients.index', compact('clients'));
+		
 	}
 
 	/**
